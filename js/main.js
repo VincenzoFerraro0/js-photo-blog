2b9d41dcdb5f0293915bedbcac59fef817558e2c
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-29 17:15:09 +0100

    I add comments

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
-document.querySelector('#card-list')
-
+// Effettua una richiesta HTTP GET all'API per ottenere un elenco di immagini
 
 fetch("https://lanciweb.github.io/demo/api/pictures/", { method: "GET" })
-    .then(response => response.json())
-    .then(data => {
+    .then(response => response.json()) // Converte la risposta in formato JSON
+    .then(data => { // Una volta ottenuti i dati, esegue questa funzione
+        
+        console.log(data); // Stampa i dati ricevuti nella console per il debug
 
-        console.log(data)
-
+        // Itera su ogni elemento (oggetto immagine) ricevuto dall'API
         data.forEach(element => {
-            console.log(element)
+            console.log(element); // Stampa l'elemento corrente nella console per il debug
 
+            // Seleziona il contenitore con id 'card-list' e aggiunge una nuova card con i dati dell'elemento
             document.querySelector('#card-list').innerHTML +=
             `
-                <div  class="recipe-card">
+                <div class="recipe-card">
                     <figure class="pin">
-                        <img width="35px" src="./img/pin.svg" alt="">
+                        <img width="35px" src="./img/pin.svg" alt="Pin">
                     </figure>
                     <div class="col">
                         <figure>
-                            <img src="${element.url}" alt="">
+                            <img src="${element.url}" alt="${element.title}">
                         </figure>
                         <span>${element.title}</span>
-                        <span>${element.data}</span>
-                        
+                        <span>${element.data}</span>   
                     </div>
                 </div>
-            
-            `
+            `;
         });
 
-
-
     })
-    .catch(error => {
-        // codice da eseguire in caso di errore
+    .catch(error => { 
+        // Se si verifica un errore nella richiesta, viene catturato e stampato nella console
         console.error(error);
     });
-
