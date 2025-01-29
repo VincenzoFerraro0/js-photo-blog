e65c6749223bceaf72864a92aa0e504c30b8123d
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-29 19:54:31 +0100

    exercise finished

--- js/main.js
+++ js/main.js
@@ -3,33 +3,33 @@
 fetch("https://lanciweb.github.io/demo/api/pictures/", { method: "GET" })
     .then(response => response.json()) // Converte la risposta in formato JSON
     .then(data => { // Una volta ottenuti i dati, esegue questa funzione
-        
+
         console.log(data); // Stampa i dati ricevuti nella console per il debug
 
-        // Itera su ogni elemento (oggetto immagine) ricevuto dall'API
+        // Itera su ogni elemento (oggetto ) ricevuto dall'API
         data.forEach(element => {
             console.log(element); // Stampa l'elemento corrente nella console per il debug
 
             // Seleziona il contenitore con id 'card-list' e aggiunge una nuova card con i dati dell'elemento
             document.querySelector('#card-list').innerHTML +=
-            `
+                `
                 <div class="recipe-card">
                     <figure class="pin">
-                        <img width="35px" src="./img/pin.svg" alt="Pin">
+                        <img src="./img/pin.svg" alt="Pin">
                     </figure>
                     <div class="col">
                         <figure>
                             <img src="${element.url}" alt="${element.title}">
                         </figure>
-                        <span>${element.title}</span>
-                        <span>${element.data}</span>   
+                        <span class="data">${element.date}</span>   
+                        <span class="title">${element.title}</span>
                     </div>
                 </div>
             `;
         });
 
     })
-    .catch(error => { 
+    .catch(error => {
         // Se si verifica un errore nella richiesta, viene catturato e stampato nella console
         console.error(error);
     });
