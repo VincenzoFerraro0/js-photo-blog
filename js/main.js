4b23622fbfbf2c349116523d26123467d817faeb
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-30 18:33:21 +0100

    exercise finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const endpoint = 'https://lanciweb.github.io/demo/api/pictures/' // Dichiaro una variabile che corrisponde al l'indirizzo dell' api 
-const methodFetch = { method: 'GET' } // dichiaro una variabile che corrisponde method: 'GET'
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/' // Dichiara la variabile con l'URL dell'API 
+const methodFetch = { method: 'GET' } // Definisce il metodo della richiesta HTTP come GET
 
 // Effettua una richiesta HTTP GET all'API per ottenere un elenco di immagini
 fetch(endpoint, methodFetch)
@@ -8,18 +8,19 @@ fetch(endpoint, methodFetch)
 
         console.log(data); // Stampa i dati ricevuti nella console per il debug
 
-        
-        // Itera su ogni elemento (oggetto ) ricevuto dall'API
+        let arrayCards = [] // Inizializza un array vuoto per contenere le card
+   
+        // Itera su ogni elemento (oggetto) ricevuto dall'API
         data.forEach(element => {
             console.log(element); // Stampa l'elemento corrente nella console per il debug
 
-            //Destructuring
-            const {date, id, title, url } = element
+            // Destructuring per estrarre le proprietà dell'oggetto
+            const { date, id, title, url } = element
 
             // Seleziona il contenitore con id 'card-list' e aggiunge una nuova card con i dati dell'elemento
             document.querySelector('#card-list').innerHTML +=
                 `
-                <div class="cards">
+                <div class="cards" data-img='${id}'>
                     <figure class="pin">
                         <img src="./img/pin.svg" alt="Pin">
                     </figure>
@@ -32,13 +33,50 @@ fetch(endpoint, methodFetch)
                     </div>
                 </div>
             `;
+
+            // Aggiorna l'array con tutte le card create
+            arrayCards = document.querySelectorAll('.cards')
         });
+
+        console.log(arrayCards) // Stampa l'array delle card per il debug
+
+        let overlay = document.querySelector('.overlay') // Seleziona l'elemento overlay
+
+        // Aggiunge un evento di click a ciascuna card
+        arrayCards.forEach(element => {
+            element.addEventListener('click', function () {
+                let dataImg = this.getAttribute('data-img') // Ottiene l'attributo data-img
+
+                overlay.style.display = 'flex' // Mostra l'overlay
+                overlay.innerHTML =
+                `
+                    <button id="btn-close" class="btn-buble">Chiudi</button>
+                    <figure>
+                        <img src="https://marcolanci.it/boolean/assets/pictures/${dataImg}.png" alt="">
+                    </figure>
+                `
+                
+                // Aggiunge un evento per chiudere la modale con il bottone
+                let btnClose = document.querySelector('#btn-close')
+                btnClose.addEventListener('click', function () {
+                    overlay.style.display = "none";
+                });
+
+                
+    
+            });
+        });
+
+
+
+        // Chiude la modale cliccando fuori dalla modale stessa
+        window.onclick = function (event) {
+            if (event.target == overlay) {
+                overlay.style.display = "none";
+            }
+        }
     })
-    
-    
     .catch(error => {
         // Se si verifica un errore nella richiesta, viene catturato e stampato nella console
         console.error(error);
     });
-
-    
